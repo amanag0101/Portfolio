d6fd26e3a80e6eeb321a7221a2299266f12c85bc
Aman Agarwal <amanag0101@gmail.com>
2023-05-22 02:50:52 +0530

    created the navbar component

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,9 +1,26 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import Link from "next/link";
 import styles from "./navbar.module.css";
 
 export default function Navbar() {
-    return (
-        <div className={styles.navbar}>
-            <h1>Navigation</h1>
-        </div>
-    );
+  const pathname = usePathname();
+
+  const isActive = (url: string) => {
+    return pathname === url ? true : false;
+  };
+
+  return (
+    <div className={styles.navbar}>
+      <div className={styles.container}>
+        <Link className={isActive("/") ? "active" : ""} href="/">
+          Home
+        </Link>
+        <Link href="#">Projects</Link>
+        <Link href="#">Experience</Link>
+        <Link href="#">Tech Stack</Link>
+      </div>
+    </div>
+  );
 }
