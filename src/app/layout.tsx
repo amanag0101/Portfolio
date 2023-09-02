85683d91407bfde924edf379333c6d414e7afd89
Aman Agarwal <amanag0101@gmail.com>
2023-09-03 02:23:17 +0530

    Added particles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,13 @@
+"use client";
+
+import SpaceParticles from "./components/particles/SpaceParticles";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import styles from "./layout.module.css";
+import { useCallback } from "react";
+import { Container, Engine } from "tsparticles-engine";
+import { loadSlim } from "tsparticles-slim";
+import Particles from "react-tsparticles";
 import "./globals.css";
 
 export default function RootLayout({
@@ -8,10 +15,27 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const particlesInit = useCallback(async (engine: Engine) => {
+    console.log(engine);
+    await loadSlim(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(
+    async (container: Container | undefined) => {
+      console.log(container);
+    },
+    []
+  );
+
   return (
     <html lang="en">
       <body>
         <div className={styles.layout}>
+          <Particles
+            options={SpaceParticles}
+            init={particlesInit}
+            loaded={particlesLoaded}
+          />
           <header className={styles.header}>
             <Header />
           </header>
